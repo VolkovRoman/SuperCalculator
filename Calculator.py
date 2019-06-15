bdf4658762afbfb81c6676b5e65d37c0fe50871b
roman <romawolf@yandex.ru>
2019-06-15 18:57:26 +0300

    Update all code. Add converter and sum.

--- Calculator.py
+++ Calculator.py
@@ -1,32 +1,53 @@
+def convert_base(num, to_base=10, from_base=10):
+    # first convert to decimal number
+    if isinstance(num, str):
+        n = int(num, from_base)
+    else:
+        n = int(num)
+    # now convert decimal to 'to_base' base
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if n < to_base:
+        return alphabet[n]
+    else:
+        return convert_base(n // to_base, to_base) + alphabet[n % to_base]
 
 
-# a = input()
-# b = input()
+a = ('1234', 10)
+b = ('98765', 10)
+c = ('01010110', 2)
+d = ('01010101', 2)
+e = ('364', 8)
+f = ('146', 8)
+g = ('8a', 16)
+h = ('0e', 16)
+
+binary_a = convert_base(a[0], 2, a[1])
+binary_b = convert_base(b[0], 2, b[1])
+binary_c = convert_base(c[0], 2, c[1])
+binary_d = convert_base(d[0], 2, d[1])
+binary_e = convert_base(e[0], 2, e[1])
+binary_f = convert_base(f[0], 2, f[1])
+binary_g = convert_base(g[0], 2, g[1])
+binary_h = convert_base(h[0], 2, h[1])
 
 
-class SuperCalculator:
-    def __init__(self, value1, prefix1, value2, prefix2):
-        self.value1 = value1
-        self.prefix1 = prefix1
-        self.value2 = value2
-        self.prefix2 = prefix2
+def sum_(x, y):
+    max_len = max(len(x), len(y))
+    x = x.zfill(max_len)
+    y = y.zfill(max_len)
+    shift = 0
+    res = ''
+    for i in range(max_len - 1, -1, -1):
+        r = shift
+        r += 1 if x[i] == '1' else 0
+        r += 1 if y[i] == '1' else 0
+        res = ('1' if r % 2 == 1 else '0') + res
+        shift = 0 if r < 2 else 1
 
-    def sum(self):
-        return self.value1+self.value2
-
-    def sub(self):
-        return self.value1-self.value2
-
-    def mul(self):
-        return self.value1*self.value2
-
-    def div(self):
-        return self.value1/self.value2
+    if shift != 0:
+        res = '1' + res
+    return res.zfill(max_len)
 
 
-num = SuperCalculator(456, 10, 2, 10)
+sum_(binary_a, binary_b)
 
-print(SuperCalculator.sum(num))
-print(SuperCalculator.sub(num))
-print(SuperCalculator.mul(num))
-print(SuperCalculator.div(num))
